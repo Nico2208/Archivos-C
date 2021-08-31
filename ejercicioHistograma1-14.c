f4b9e1d2288325b14b81b464e02fcae2c82cbbbb
Nicolas Sicolo <sicolo2208@gmail.com>
2021-08-31 16:44:31 -0300

    agregado histograma

---
+++ ejercicioHistograma1-14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int c, numWord;
+    int vWords[5];
+
+    for (int i = 0; i < 5; i++){
+        numWord=0;
+        while( (c = getchar()) != EOF){
+            if (c != '\n')
+                ++numWord;
+        }
+        vWords[i] = numWord;
+    }
+
+    printf("Histograma\n");
+
+    for (int i = 0; i < 5; ++i){
+        for (int j = 0; j < vWords[i]; j++)
+            printf("-");
+        printf("\n");
+    }
+}
